e3b1fde3972cecb9d605209bbe6031ff699aae9e
JustAnEric <73326807+JustAnEric@users.noreply.github.com>
2023-07-02 16:42:38 +1000

    Update make.sh

--- installation/make.sh
+++ installation/make.sh
@@ -4,8 +4,12 @@ titlescreen="""
 Make.sh Utility - 2023
 """;
 defaultsudopathonlinux="/usr/bin/sudo";
+defaultbashpathonlinux="/bin/bash"
 echo $titlescreen;
 if [ $1=="--make-sudo" ] then
   echo "Copying sudo [/usr/bin/sudo] and modifying..." && sudo cp $defaultsudopathonlinux .;
+fi;
+if [ $1=="--make-bash" ] then
+  echo "Copying bash/sh [/bin/bash] and modifying..." && sudo cp $defaultbashpathonlinux .;
 fi;
 echo "Make.sh utility job has finished."
